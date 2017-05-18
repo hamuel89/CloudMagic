4cd1634ef7cb667a6bf68efc9678e8d780d0e99d
Scottishdwarf <jarno.matikainen@glaciertv.com>
2017-05-18 19:18:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//////////////////////////////////////////////////
+//                                              //
+//   See License.txt for Licensing information  //
+//                                              //
+//////////////////////////////////////////////////
+
+using System;
+using System.Windows.Forms;
+using CloudMagic.GUI.GUI;
+
+namespace CloudMagic.GUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
